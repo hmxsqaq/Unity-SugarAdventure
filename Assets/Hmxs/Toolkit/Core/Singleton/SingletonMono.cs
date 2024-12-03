a5e30f8c84ef20cbf8aa2bf49392e4a73b7cff14
hmxs <hmxsqaq@gmail.com>
2024-12-03 13:44:44 +0800

    [init]

---
+++ Assets/Hmxs/Toolkit/Core/Singleton/SingletonMono.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hmxs.Toolkit
+{
+    /// <summary>
+    /// generic singleton base class - inherit from MonoBehaviour
+    /// </summary>
+    public abstract class SingletonMono<T> : MonoBehaviour where T : SingletonMono<T>
+    {
+        private static readonly Lazy<T> InstanceHolder = new(() => {
+            var instance = FindObjectOfType<T>();
+            if (instance != null) return instance;
+            var singletonObject = new GameObject(typeof(T).Name + "_Singleton");
+            instance = singletonObject.AddComponent<T>();
+            instance.OnInstanceInit(instance);
+            return instance;
+        });
+
+        public static T Instance => InstanceHolder.Value;
+
+        /// <summary>
+        /// called when instance is initialized
+        /// </summary>
+        protected virtual void OnInstanceInit(T instance) => DontDestroyOnLoad(gameObject);
+
+        protected virtual void Awake()
+        {
+            if (InstanceHolder.IsValueCreated && InstanceHolder.Value != this) Destroy(gameObject);
+        }
+    }
+}
