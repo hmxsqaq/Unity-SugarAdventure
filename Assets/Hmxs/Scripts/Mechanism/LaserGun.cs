6c92a0a0c42cbbb832d9de75cba587f29fa300a7
hmxs <hmxsqaq@gmail.com>
2024-12-17 00:22:27 +0800

    [feat] update laser gun & ledge

--- Assets/Hmxs/Scripts/Mechanism/LaserGun.cs
+++ Assets/Hmxs/Scripts/Mechanism/LaserGun.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using MoreMountains.Feedbacks;
 using UnityEngine;
 
 namespace Hmxs.Scripts.Mechanism
@@ -6,12 +7,21 @@ namespace Hmxs.Scripts.Mechanism
 	public class LaserGun : MonoBehaviour
 	{
 		[SerializeField] private Laser laser;
-
+		[SerializeField] private float startDelay;
 		[SerializeField] private float activationTime;
 		[SerializeField] private float deactivationTime;
+		[SerializeField] private MMF_Player gunChargingFeedback;
 
-		private void Start()
+		private IEnumerator Start()
 		{
+			yield return new WaitForSeconds(startDelay);
+			gunChargingFeedback.PlayFeedbacks();
+			var counter = 0f;
+			while (counter < deactivationTime)
+			{
+				counter += Time.deltaTime;
+				yield return null;
+			}
 			StartCoroutine(LaserEnableCoroutine());
 		}
 
@@ -30,6 +40,7 @@ namespace Hmxs.Scripts.Mechanism
 		private IEnumerator LaserDisableCoroutine()
 		{
 			DeactivateLaser();
+			gunChargingFeedback.PlayFeedbacks();
 			var counter = 0f;
 			while (counter < deactivationTime)
 			{
