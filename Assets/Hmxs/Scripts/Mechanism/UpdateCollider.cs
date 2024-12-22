083aa7e05393c521cc6af056fa8e8e8d83cfd495
hmxs <hmxsqaq@gmail.com>
2024-12-23 04:15:21 +0800

    [feat] 完成障碍物，加热器

---
+++ Assets/Hmxs/Scripts/Mechanism/UpdateCollider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Hmxs.Scripts.Mechanism
+{
+	public class UpdateCollider : MonoBehaviour
+	{
+		[Button]
+		private void Start()
+		{
+			GetComponent<BoxCollider2D>().size = GetComponent<SpriteRenderer>().size;
+		}
+	}
+}
